be8c6e50055c7524587a656982b401f5b923816f
DKarandikar <daniel.karandikar@zoho.com>
2020-11-18 22:22:58 +0000

    Add 1-2
    Slower than I would have liked, elem is pretty slow as the list grows
    Keeping a set as we go along would have been nicer

--- src/days/Day1.hs
+++ src/days/Day1.hs
@@ -12,5 +12,16 @@ handleLine (c:cs)
     | c == '-' = (0 - (read cs :: Int))
     | otherwise = error $ "Invalid input: " ++ c:cs
 
+
+-- part2 i = show $ listStartInList [20,30,21,28,25,32,16,2,10,13,0]
+-- part2 i = show $ iterate (accumulateLines (lines i)) [0] !! 100000
+-- part2 i = show $ accumulateLines (lines i) [0]
+
 part2:: String -> String
-part2 i = i
+part2 i = show $ head $ until listStartInList (accumulateLines $ lines i) [0]
+
+listStartInList :: [Int] -> Bool
+listStartInList (c:cs) = elem c cs
+
+accumulateLines :: [String] -> [Int] -> [Int]
+accumulateLines lines l = (head l) + handleLine (lines!!((length l - 1) `mod` (length lines))) : l
