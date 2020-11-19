18d2fe5563423a795d48a0d0999dd4bb2c39ca0c
DKarandikar <daniel.karandikar@zoho.com>
2020-11-19 23:42:41 +0000

    Add comments into Day3Spec
    Add some details on what the test case is

--- tst/unit/Day3Spec.hs
+++ tst/unit/Day3Spec.hs
@@ -13,3 +13,23 @@ spec = do
         d3_1 "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2" `shouldBe` "4"
     it "d3_2 handles simple example" $ do
         d3_2 "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2" `shouldBe` "#3"
+
+-- c.f. https://adventofcode.com/2018/day/3
+
+-- #1 @ 1,3: 4x4
+-- #2 @ 3,1: 4x4
+-- #3 @ 5,5: 2x2
+-- Visually, these claim the following areas:
+
+-- ........
+-- ...2222.
+-- ...2222.
+-- .11XX22.
+-- .11XX22.
+-- .111133.
+-- .111133.
+-- ........
+
+-- The four square inches marked with X are claimed by both 1 and 2
+
+-- Thus, there are 4 overlapping areas, and #3 is the only non-overlapping claim
